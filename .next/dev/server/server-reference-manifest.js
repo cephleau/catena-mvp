2141773d000a777e7c7bfe334d49a8a7b9f4de40
Carlos Cadena <cephleau@gmail.com>
2026-08-30 22:55:40 -0600

    chore: ignore .next build directory

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vPQhBzZjRrJ/ShwhmCl11a4Kf3zDFJelLEWkF8y3AcI=\"\n}"
--- .next/server/chunks/ssr/[root-of-the-server]__0eimlx7._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[18622,(a,b,c)=>{b.exports=a.x("next/dist/compiled/next-server/app-page-turbo.runtime.prod.js",()=>require("next/dist/compiled/next-server/app-page-turbo.runtime.prod.js"))},33354,(a,b,c)=>{"use strict";c._=function(a){return a&&a.__esModule?a:{default:a}}},42602,(a,b,c)=>{"use strict";b.exports=a.r(18622)},35112,(a,b,c)=>{"use strict";b.exports=a.r(42602).vendored["react-ssr"].ReactDOM},87924,(a,b,c)=>{"use strict";b.exports=a.r(42602).vendored["react-ssr"].ReactJsxRuntime},72131,(a,b,c)=>{"use strict";b.exports=a.r(42602).vendored["react-ssr"].React}];
-
-//# sourceMappingURL=%5Broot-of-the-server%5D__0eimlx7._.js.map
